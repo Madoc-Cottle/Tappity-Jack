ad739015ecc21f9d6dd7fe9b337862860eea13d9
Pydronia <jackscmail@gmail.com>
2017-05-31 11:49:06 +1000

    Add full basic functionality and color changing

--- Tappity/ViewController.swift
+++ Tappity/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var myView: UIView!
+    
     @IBOutlet weak var tapButton: UIButton!
     @IBOutlet weak var userScore: UILabel!
     @IBOutlet weak var timeRemaining: UILabel!
@@ -20,18 +25,69 @@ class ViewController: UIViewController {
     @IBOutlet weak var tapHozCenter: NSLayoutConstraint!
     @IBOutlet weak var tapVertCenter: NSLayoutConstraint!
     
+    // MARK: - Globals
+    
+    var firstTap = true
+    var timeCounter = 30
+    var score = 0
+    
+    let colorCycle = [UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow, UIColor.purple, UIColor.brown, UIColor.orange, UIColor.white, UIColor.cyan, UIColor.gray]
+    
+    //MARK: - Functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //set the tap button to be a quater of the screen size
-        tapHeightConstraint.constant = 250
-        tapWidthConstraint.constant = 20
+        let screenBounds = UIScreen.main.bounds
+        let buttonSize = 0.25 * screenBounds.width
+        tapHeightConstraint.constant = buttonSize
+        tapWidthConstraint.constant = buttonSize
+        
+        //set labels correctly
+        timeRemaining.text = "\(timeCounter)"
+        userScore.text = "\(score)"
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        if (firstTap) {
+            firstTap = false
+            //count down
+            let _ = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+                self.timeCounter -= 1
+                self.timeRemaining.text = "\(self.timeCounter)"
+                
+                if (self.timeCounter == 0){
+                    timer.invalidate()
+                    self.finishedGame()
+                }
+                
+            }
+        } else {
+            score += 1
+            userScore.text = "\(score)"
+            myView.backgroundColor = colorCycle[Int(arc4random_uniform(UInt32(colorCycle.count)))]
+        }
+        
     }
+    
+    func finishedGame(){
+        let alert = UIAlertController(title: "Game Over", message: "Your score was \(score)", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .destructive, handler: nil))
+        self.present(alert, animated: true, completion: resetGame)
+    }
+    
+    func resetGame(){
+        score = 0
+        timeCounter = 30
+        firstTap = true
+        timeRemaining.text = "\(timeCounter)"
+        userScore.text = "\(score)"
+    }
+    
 
 
 }
